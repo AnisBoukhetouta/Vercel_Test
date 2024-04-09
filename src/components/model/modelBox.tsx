12116b680a59a52335b96537fa3ad7524f67243f
tommy20x <ToomyHong1913@outlook.com>
2024-04-09 13:15:26 +0845

    background align top

--- src/components/model/modelBox.tsx
+++ src/components/model/modelBox.tsx
@@ -25,6 +25,7 @@ export default function ModelBox() {
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
+          backgroundPositionY: "Top",
         }}
         shadows
       >
