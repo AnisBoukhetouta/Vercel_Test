036daa93d9ca4c2cef6a9d5a854609b35c1b3055
dilberkov <dilberkov609@gmail.com>
2024-04-15 09:19:44 +0900

    add the gameloby page

--- src/navigation/navlinks/navlinks.tsx
+++ src/navigation/navlinks/navlinks.tsx
@@ -1,15 +1,12 @@
 import * as React from "react";
 import classes from "./navlinks.module.css";
 import Navlink from "../navlink/navlink";
-import { NavLink } from "react-router-dom";
 
 const navlinks = () => {
   return (
     <div>
       <ul className={classes.navlinks}>
-        <NavLink to="/" style={{ textDecoration: "none" }}>
-          <h1 className={classes.title}>LexSCHOOL</h1>
-        </NavLink>
+
         <Navlink link="/">Home</Navlink>
         <Navlink link="/gamelobby">GameLobby</Navlink>
         <Navlink link="/upload">Upload</Navlink>
--- src/pages/gameLobby/gameLobby.tsx
+++ src/pages/gameLobby/gameLobby.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./gameLobby.module.css";
 import LobbyHeader from "../../components/lobbyHeader/lobbyHeader";
 import LobbyHeaderGame from "../../components/lobbyHeaderGame/lobbyHeaderGame";
 
 export default function GameLobby() {
+  const [item, setItem] = useState<any>();
+
   return (
     <div className={classes.gameLobbyContainer}>
       <LobbyHeader />
-      <LobbyHeaderGame />
+      <LobbyHeaderGame setItem={setItem} />
     </div>
   );
 }
