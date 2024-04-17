f9a3a40e3fd6ad948c262778b2ce99b87a15e3b7
Hexzilla <hexzilla256@gmail.com>
2024-04-17 18:08:43 +0845

    Add skeleton

--- src/components/gameCards/gameCard.tsx
+++ src/components/gameCards/gameCard.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import classes from "./card.module.css";
 import CardData from "./gameCardData";
+import { Skeleton } from "@mui/material";
 
 interface Props {
   onSetItem?: (e: any) => void;
@@ -17,22 +18,26 @@ export default function GameCard({ item, onSetItem, link }: Props) {
 
   return (
     <div className={classes.cardBody}>
-      <div
-        className={classes.center}
-        key={item._id}
-        onClick={() => handleClick(!mouseOver)}
-        onMouseOver={() => setMouseOver(true)}
-        onMouseOut={() => setMouseOver(false)}
-      >
-        <div className={classes.card}>
-          <img
-            src={item.imageOver}
-            alt="game"
-            className={classes.enlargeImage}
-          />
+      {item ? (
+        <div
+          className={classes.center}
+          key={item._id}
+          onClick={() => handleClick(!mouseOver)}
+          onMouseOver={() => setMouseOver(true)}
+          onMouseOut={() => setMouseOver(false)}
+        >
+          <div className={classes.card}>
+            <img
+              src={item.imageOver}
+              alt="game"
+              className={classes.enlargeImage}
+            />
+          </div>
+          {!link && <CardData item={item} />}
         </div>
-        {!link && <CardData item={item} />}
-      </div>
+      ) : (
+        <Skeleton variant="rectangular" width={235} height={!link?198:120}/>
+      )}
     </div>
   );
 }
