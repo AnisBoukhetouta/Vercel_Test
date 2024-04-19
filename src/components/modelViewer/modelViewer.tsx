3833971f0d620a3b09466987d3cfff00c6a23cf0
Hexzilla <hexzilla256@gmail.com>
2024-04-19 11:55:42 +0845

    Update ModelViewer Component

--- src/components/modelViewer/modelViewer.tsx
+++ src/components/modelViewer/modelViewer.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 import * as THREE from "three";
+import classes from "./modelViewer.module.css";
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { OrbitControls, useAnimations } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-
-function Model() {
-  const { camera } = useThree();
-  const gltf = useLoader(GLTFLoader, "models/character0.glb");
-  const { animations, nodes } = gltf;
-  const mixer = new THREE.AnimationMixer(nodes.Scene);
-
-  const clip = animations[0];
-  const action = mixer.clipAction(clip);
-  useFrame((state, delta) => {
-    mixer.update(delta);
-  });
-  action.play();
-  action.loop = THREE.LoopRepeat;
-  camera.position.set(0, 0, 10);
-
-  //   camera.fov = 45; // Set the FOV (in degrees) for the zoom level
-
-  camera.lookAt(0, 0, 0);
-  return <primitive object={nodes.Scene} position={[0, -1, 8.2]} />;
-}
 
 interface Props {
   src: string;
 }
 
 function ModelViewer({ src }: Props) {
+  function Model() {
+    const { camera } = useThree();
+    const gltf = useLoader(GLTFLoader, "models/character0.glb");
+    // const gltf = useLoader(GLTFLoader, src);
+    const { animations, nodes } = gltf;
+    const mixer = new THREE.AnimationMixer(nodes.Scene);
+
+    const clip = animations[0];
+    const action = mixer.clipAction(clip);
+    useFrame((state, delta) => {
+      mixer.update(delta);
+    });
+    action.play();
+    action.loop = THREE.LoopRepeat;
+    camera.position.set(0, 0, 10);
+
+    //   camera.fov = 45; // Set the FOV (in degrees) for the zoom level
+
+    camera.lookAt(0, 0, 0);
+    return <primitive object={nodes.Scene} position={[0, -1, 8.2]} />;
+  }
+
   return (
-    <Canvas style={{ width: "100vw", height: "100vh" }}>
+    <Canvas className={classes.modelContainer}>
       <ambientLight intensity={1} />
       <directionalLight
         intensity={0.8}
