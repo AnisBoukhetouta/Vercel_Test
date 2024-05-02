f555df7f1ee048a411116c94836d905d229e2262
Hexzilla <hexzilla256@gmail.com>
2024-05-02 15:48:56 +0845

    Initializing... with Minimal-Client

--- next.config.js
+++ next.config.js
@@ -1,37 +1,21 @@
-// Remove this if you're not using Fullcalendar features
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid',
-  '@fullcalendar/timeline'
-]);
-
-const path = require('path');
-
 module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+  trailingSlash: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/lab': {
+      transform: '@mui/lab/{{member}}',
+    },
   },
-}
-
-module.exports = withTM({
-  reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack']
+      use: ['@svgr/webpack'],
     });
     return config;
   },
-  async redirects() {
-    return [
-      {
-        source: '/docs',
-        destination: '/docs/welcome',
-        permanent: true
-      }
-    ];
-  }
-});
+};
