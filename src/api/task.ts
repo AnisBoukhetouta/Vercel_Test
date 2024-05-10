13e320355b8fc5f4d34cfb007546638778decd2e
Hexzilla <hexzilla256@gmail.com>
2024-05-10 17:43:46 +0900

    Fix task useSWR

--- src/api/task.ts
+++ src/api/task.ts
@@ -1,13 +1,13 @@
 import useSWR from 'swr';
 import { useMemo } from 'react';
 
-import { fetcher, endpoints } from 'src/utils/axios';
+import { fetcher2, endpoints } from 'src/utils/axios';
 
 const URL = `https://grat.fun/api/pwniq${endpoints.taskTimer}`;
 
 export function useGetTimer(userId: string) {
   const GET_URL = `${URL}?uid=${userId}`;
-  const { data, isLoading, error, isValidating } = useSWR(GET_URL, fetcher);
+  const { data, isLoading, error, isValidating } = useSWR(GET_URL, fetcher2);
 
   const memoizedValue = useMemo(
     () => ({ timer: data?.timer, isLoading, error, isValidating }),
