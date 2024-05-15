fc4465ae21cd8e0913bb5f976376ae7ed6441c62
Hexzilla <hexzilla256@gmail.com>
2024-05-15 19:24:03 +0900

    Updated Layout with Auth

--- src/app/create/layout.tsx
+++ src/app/create/layout.tsx
@@ -5,6 +5,7 @@ import { useScroll } from 'framer-motion';
 import { Box, alpha } from '@mui/material';
 
 import MainLayout from 'src/layouts/main';
+import { AuthGuard } from 'src/auth/guard';
 
 import ScrollProgress from 'src/components/scroll-progress';
 
@@ -18,23 +19,25 @@ export default function Layout({ children }: Props) {
   const { scrollYProgress } = useScroll();
 
   return (
-    <MainLayout>
-      <ScrollProgress scrollYProgress={scrollYProgress} />
-      <Box
-        component="div"
-        sx={{
-          m: 3,
-          p: '40px',
-          height: '85vh',
-          overflow: 'scroll',
-          scrollbarWidth: 'none',
-          borderRadius: 2,
-          bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
-          border: (theme) => `dashed 1px ${theme.palette.divider}`,
-        }}
-      >
-        {children}
-      </Box>
-    </MainLayout>
+    <AuthGuard>
+      <MainLayout>
+        <ScrollProgress scrollYProgress={scrollYProgress} />
+        <Box
+          component="div"
+          sx={{
+            m: 3,
+            p: '40px',
+            height: '85vh',
+            overflow: 'scroll',
+            scrollbarWidth: 'none',
+            borderRadius: 2,
+            bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
+            border: (theme) => `dashed 1px ${theme.palette.divider}`,
+          }}
+        >
+          {children}
+        </Box>
+      </MainLayout>
+    </AuthGuard>
   );
 }
