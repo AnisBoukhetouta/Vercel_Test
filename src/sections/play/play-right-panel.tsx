9fe105e29d6b46a00b30ef6952eaf47b3f1316ae
Hexzilla <hexzilla256@gmail.com>
2024-05-27 13:50:07 +0900

    Updated play page userName

--- src/sections/play/play-right-panel.tsx
+++ src/sections/play/play-right-panel.tsx
@@ -1,4 +1,8 @@
+import React from 'react';
+
 import { Paper, Stack, Typography } from '@mui/material';
+
+import { useAuthContext } from 'src/auth/hooks';
 
 import { CustomStepper } from 'src/components/custom-stepper';
 
@@ -6,6 +10,13 @@ import PlayProgresses from './play-progresses';
 import PlayFeatureSorted from './play-feature-sorted';
 
 export default function PlayRightPanel() {
+  const { user } = useAuthContext();
+  const [userName, setUserName] = React.useState('');
+
+  React.useEffect(() => {
+    setUserName(user?.displayName);
+  }, [user]);
+
   return (
     <Paper
       elevation={3}
@@ -18,7 +29,7 @@ export default function PlayRightPanel() {
       <Stack direction="row" justifyContent="space-between" gap={2} height="100%">
         <Stack direction="column" justifyContent="space-between" width="60%">
           <Typography sx={{ fontSize: '18px', lineHeight: '28px', fontWeight: 700 }}>
-            [Username]
+            {userName}
           </Typography>
           <PlayProgresses />
           <CustomStepper />
