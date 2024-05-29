f13d7dfa96c52d65cf4178faf0ae0a2606849449
Hexzilla <hexzilla256@gmail.com>
2024-05-29 14:16:32 +0900

    removed extra files

--- src/sections/dashboard/dashboard-parent/parent-apex-chart/index.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const ParentApexChart = dynamic(() => import('./parent-apex-chart'), { ssr: false });
-
-export default ParentApexChart;
--- src/sections/dashboard/dashboard-player/player-apex-chart/index.tsx
+++
@@ -1,5 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const PlayerApexChart = dynamic(() => import('./player-apex-chart'), { ssr: false });
-
-export default PlayerApexChart;
