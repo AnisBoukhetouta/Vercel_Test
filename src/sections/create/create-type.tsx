7e56f526e2b32fe7efcc272054f0380ad0bf042e
Hexzilla <hexzilla256@gmail.com>
2024-06-05 01:59:06 +0900

    Create-Type Responsive

--- src/sections/create/create-type.tsx
+++ src/sections/create/create-type.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useFormikContext } from 'formik';
 
 import Radio from '@mui/material/Radio';
-import { Stack, RadioGroup, FormControlLabel } from '@mui/material';
+import { Grid, RadioGroup, FormControlLabel } from '@mui/material';
 
 import { GameType } from 'src/constants';
 
@@ -13,38 +13,61 @@ import { SmallRequiredTypography } from 'src/components/custom-typo/custom-typo'
 export default function CreateType() {
   const { handleChange } = useFormikContext();
   return (
-    <Stack direction="row" alignItems="center" gap={1}>
-      <SmallRequiredTypography title="GAME TYPE" />
-      <RadioGroup
-        name="gameType"
-        onChange={handleChange}
-        row
-        // defaultValue="first"
-        sx={{ gap: 1, maxWidth: '880px' }}
-      >
-        <FormControlLabel value={GameType.first} control={<Radio size="medium" />} label="Main" />
-        <FormControlLabel
-          value={GameType.second}
-          control={<Radio size="medium" />}
-          label="Speed Mini-Game"
-        />
-        <FormControlLabel
-          value={GameType.third}
-          control={<Radio size="medium" />}
-          label="Shield Mini-Game"
-        />
-        <FormControlLabel
-          value={GameType.fourth}
-          control={<Radio size="medium" />}
-          label="Endurance Mini-Game"
-        />
-        <FormControlLabel value={GameType.fifth} control={<Radio size="medium" />} label="Course" />
-        <FormControlLabel
-          value={GameType.sixth}
-          control={<Radio size="medium" />}
-          label="Other Mini-Game"
-        />
-      </RadioGroup>
-    </Stack>
+    <Grid container>
+      <Grid item xs={1.5}>
+        <SmallRequiredTypography title="GAME TYPE" />
+      </Grid>
+      <Grid item xs={10.5}>
+        <RadioGroup
+          name="gameType"
+          onChange={handleChange}
+          row
+          // defaultValue="first"
+        >
+          <Grid item sm={4} md={3} lg={2}>
+            <FormControlLabel
+              value={GameType.first}
+              control={<Radio size="medium" />}
+              label="Main"
+            />
+          </Grid>
+          <Grid item sm={4} md={3} lg={2}>
+            <FormControlLabel
+              value={GameType.second}
+              control={<Radio size="medium" />}
+              label="Speed Mini-Game"
+            />
+          </Grid>
+          <Grid item sm={4} md={3} lg={2}>
+            <FormControlLabel
+              value={GameType.third}
+              control={<Radio size="medium" />}
+              label="Shield Mini-Game"
+            />
+          </Grid>
+          <Grid item sm={4} md={3} lg={2}>
+            <FormControlLabel
+              value={GameType.fourth}
+              control={<Radio size="medium" />}
+              label="Endurance Mini-Game"
+            />
+          </Grid>
+          <Grid item sm={4} md={3} lg={2}>
+            <FormControlLabel
+              value={GameType.fifth}
+              control={<Radio size="medium" />}
+              label="Course"
+            />
+          </Grid>
+          <Grid item sm={4} md={3} lg={2}>
+            <FormControlLabel
+              value={GameType.sixth}
+              control={<Radio size="medium" />}
+              label="Other Mini-Game"
+            />
+          </Grid>
+        </RadioGroup>
+      </Grid>
+    </Grid>
   );
 }
