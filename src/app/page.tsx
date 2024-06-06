d13d917ff86aa04b461e7fe6b0f4a019250ac64c
Hexzilla <hexzilla256@gmail.com>
2024-06-06 15:10:04 +0900

    Instead Play layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Box } from '@mui/material';
+
 import { PlayView } from 'src/sections/play/view';
 
 //----------------------------------------------------------------
@@ -5,8 +7,12 @@ import { PlayView } from 'src/sections/play/view';
 export const metadata = {
   title: 'Play',
   description: 'Play the game',
-}
+};
 
 export default function PlayPage() {
-  return <PlayView />;
+  return (
+    <Box component="div" sx={{ p: 4, width: '100%', height: '100vh' }}>
+      <PlayView />
+    </Box>
+  );
 }
