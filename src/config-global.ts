e32205d4bb0469d37e9bd68658679cf6575a97e5
Hexzilla <hexzilla256@gmail.com>
2024-06-08 15:59:58 +0900

    changed login route

--- src/config-global.ts
+++ src/config-global.ts
@@ -38,4 +38,5 @@ export const SUPABASE_API = {
 export const MAPBOX_API = process.env.NEXT_PUBLIC_MAPBOX_API;
 
 // ROOT PATH AFTER LOGIN SUCCESSFUL
-export const PATH_AFTER_LOGIN = paths.dashboard.root; // as '/dashboard'
+// export const PATH_AFTER_LOGIN = paths.dashboard.root; // as '/dashboard'
+export const PATH_AFTER_LOGIN = paths.auth.firebase.login; // as '/dashboard'
